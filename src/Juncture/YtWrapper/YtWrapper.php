a25ecf6d0af2d4c5f7d82400ad10e1f42bf058a5
Dave <codeM0nK3Y@me.com>
2013-09-19 21:22:13 +0100

    Get subscriptions working

--- src/Juncture/YtWrapper/YtWrapper.php
+++ src/Juncture/YtWrapper/YtWrapper.php
@@ -104,13 +104,76 @@ class YtWrapper {
 		return true;
 	}
 
-	public function subscriptions()
+	public function where()
 	{
-		if (is_null($this->subscriptions))
+		switch (func_num_args())
 		{
-			$this->subscriptions = new Subscriptions();
+			case 0:
+			{
+				throw new Exception("Not enough arguments for where()", 1);
+				break;
+			}
+			case 1:
+			{
+				$this->setFilters(func_get_arg(0));
+				break;
+			}
+			case 2:
+			{
+				$key = func_get_arg(0);
+				$this->setFilters([$key => func_get_arg(1)]);
+
+				unset($key);
+				break;
+			}
+			default:
+				throw new Exception("Too many arguments for where()", 1);
 		}
 
-		return $this->subscriptions;
+		return $this;
+	}
+
+	private function setFilters($filters)
+	{
+		if ( ! is_array($filters))
+		{
+			throw new Exception('setFilters requires a key=>val array, '.gettype($filters), 1);
+		}
+
+		foreach ($filters as $filter => $value)
+		{
+			$this->setFilter($filter, $value);
+		}
+
+		return $this;
+	}
+
+	private function setFilter($filter, $value)
+	{
+		$this->filters[$filter] = $value;
+
+		return $this;
+	}
+
+	public function get($dataTypes)
+	{
+		if (empty($this->filters))
+		{
+			throw new Exception("Subscriptions: Missing required search filters", 1);
+
+		}
+
+		$options = $this->filters;
+
+		$service = $this->yt->{$this->resource};
+
+		return $service->listSubscriptions($dataTypes, $options);
+	}
+
+	public function subscriptions()
+	{
+		$this->resource = 'subscriptions';
+
+		return $this;
 	}
 }
